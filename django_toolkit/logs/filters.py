0a462583c188d0145df5c0fdcb4fe0fea33734d3
Renan Ivo <renanivom@gmail.com>
2016-07-24 19:27:24 -0300

    create a docstring to AddHostName filter class

--- django_toolkit/logs/filters.py
+++ django_toolkit/logs/filters.py
@@ -4,6 +4,10 @@ import socket
 
 
 class AddHostName(logging.Filter):
+    """
+    Add the %(hostname)s entry to log record, so that it can be included in the
+    logs using a formatter
+    """
     hostname = socket.gethostname()
 
     def filter(self, record):
