f97a1ffa60be88f141709170bb01bfd14613ec4c
Renan Ivo <renanivom@gmail.com>
2016-08-04 16:31:45 -0300

    create shortcut module docs

--- django_toolkit/shortcuts.py
+++ django_toolkit/shortcuts.py
@@ -1,7 +1,7 @@
 def get_oauth2_app(request):
     """
     Return the Application object from a Django Rest Framework request when
-    authenticated with a Django OAuth Toolkit access token, and return None
+    authenticated with a Django OAuth Toolkit access token, or return None
     when it is not
     """
     try:
--- docs/index.md
+++ docs/index.md
@@ -4,7 +4,8 @@ Welcome to Django Toolkit
 Django Toolkit is the set of useful tools we use at LuizaLabs to develop
 projects using the Django Framework
 
-This package includes tools for:
+This package includes the utility modules:
 
 * [logs](logs)
 * [middlewares](middlewares)
+* [shortcuts](shortcuts)
