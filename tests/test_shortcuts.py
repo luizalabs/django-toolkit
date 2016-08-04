0ea79abebcf01fc06f8c6b576eb147132cc54abc
Renan Ivo <renanivom@gmail.com>
2016-08-04 15:10:17 -0300

    create shortcut to retrieve the OAuth2 application of a request

---
+++ tests/test_shortcuts.py
@@ -0,0 +1,35 @@
+import pytest
+from mixer.backend.django import mixer
+from oauth2_provider.models import AccessToken, Application
+from rest_framework.request import Request
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from django_toolkit import shortcuts
+
+
+@pytest.mark.django_db
+class TestGetCurrentApp(object):
+
+    @pytest.fixture
+    def token(self):
+        return mixer.blend(AccessToken)
+
+    def test_should_return_the_client_applicaton(self, token):
+        factory = APIRequestFactory()
+        request = factory.get('/')
+        force_authenticate(request, token=token)
+        rest_request = Request(request)
+
+        app = shortcuts.get_oauth2_app(rest_request)
+
+        assert isinstance(app, Application)
+        assert app == token.application
+
+    def test_should_return_none_when_not_authenticated(self):
+        factory = APIRequestFactory()
+        request = factory.get('/')
+        rest_request = Request(request)
+
+        app = shortcuts.get_oauth2_app(rest_request)
+
+        assert app is None
