9cd3f1172493ab0635c9157ce91e2d3f9e379ba0
Renan Ivo <renanivom@gmail.com>
2016-08-04 14:40:23 -0300

    setup tests with django database, DRF and OAuth toolkit

--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,25 @@
+SECRET_KEY = 'test'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+MIDDLEWARE_CLASSES = (
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'oauth2_provider',
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+    ),
+}
+
 TOOLKIT = {
     'API_VERSION': '1.2.3',
 }
-SECRET_KEY = 'test'
