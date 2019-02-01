bb2c1c6892d4e41c13cd9d642a3dfade84e43813
igoryossugo <iyossugo7@gmail.com>
2019-02-01 17:46:28 -0200

    add log of total requests on circuit breaker

--- django_toolkit/fallbacks/circuit_breaker/rules.py
+++ django_toolkit/fallbacks/circuit_breaker/rules.py
@@ -89,11 +89,13 @@ class PercentageFailuresRule(Rule):
             'max failures {max_failures_percentage}% - '
             'total failures {total_failures} - '
             'min accepted requests {min_accepted_requests} - '
+            'total requests {total_requests} - '
             'percentage failures {percentage_failures}%'.format(
                 key=self.failure_cache_key,
                 max_failures_percentage=self.max_failures_percentage,
                 total_failures=total_failures,
                 min_accepted_requests=self.min_accepted_requests,
+                total_requests=total_requests,
                 percentage_failures=self._get_percentage_failures(
                     total_failures=total_failures,
                     total_requests=total_requests
